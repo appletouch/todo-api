f77eda94e56ca6fb4d0ca0215e0042cdb907ba7c
Peter van de Koolwijk <kooltjes@yahoo.com>
2015-11-06 23:10:41 +0100

    This is the initial Commit for the todo-api.

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 /**
  * Created by PKoolwijk on 1-11-2015.
  */
-var express =require('express')
-//Hello this is a test
+var express =require('express');
+var app = express();
+var PORT = process.env.PORT||3000;
+
+app.get('/',function(req,res){
+    res.send('Peter\'s ToDo API root');
+
+
+});
+
+app.listen(PORT, function(){
+    console.log('Listening on port: ' + PORT)
+
+})
+
